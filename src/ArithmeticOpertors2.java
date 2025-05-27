af6e5ac4398ebe022fcc14b5ee43697aebcd5570
DurgaGaneshMedisetti <ganeshmsvd4@gmail.com>
2025-05-27 18:36:06 +0530

    ArithmeticOpertors2

---
+++ src/ArithmeticOpertors2.java
@@ -0,0 +1,18 @@
+
+public class ArithmeticOpertors2 {
+public static void main (String ar[])
+	
+	{
+	 byte x=5;
+     byte y=7;	 
+	 
+	      System.out.println("Result of addition is:"+(x+y));
+		  System.out.println("Result of sub is:"+(x-y));
+		  System.out.println("Result of mul is:"+(x*y));
+		  System.out.println("Result of div is:"+(x/y));
+		  System.out.println("Result of modulus is:"+(x%y));
+		
+		
+		}
+
+}
