65cea1896798cfd2a18ba9b42bab5fe1a7a4f9ff
MSudhakar517 <charancherry9833@gmail.com>
2025-05-28 10:25:19 +0530

    code changes for String Handlers for Regex

---
+++ src/com/demo/stringhandler/RegexDemo1.java
@@ -0,0 +1,19 @@
+package com.demo.stringhandler;
+
+public class RegexDemo1 {
+	
+	public static void main(String[] args) {
+		
+     String name = "Sudha";
+     
+ 
+     boolean matches = name.matches("^[a-zA-Z]+$");
+     
+     System.out.println(matches);
+		
+		
+		
+	}
+}
+	
+	
