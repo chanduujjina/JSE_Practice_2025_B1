eec62ea69ec06413cc189b664eb1e2a7d881e0f3
Marri Rajasekhar <rajasekhar9493748647@gmail.com>
2025-05-29 08:43:44 +0530

    Code chnages for Control statements demo

---
+++ src/com/demo/cs/oddeven.java
@@ -0,0 +1,18 @@
+package com.demo.cs;
+
+public class oddeven {
+	public static void main(String args[]) {
+	    int num = 11;
+	    
+	    if (num%2==0)
+	    {
+	        System.out.println("even numb");
+	    }
+	    else 
+	    {
+	        System.out.println("odd numb");
+	    }
+	    
+
+	}
+}
