72d1cb4f8140aa1d057fccddade0f4498e27293b
DurgaGaneshMedisetti <ganeshmsvd4@gmail.com>
2025-06-05 17:58:43 +0530

    ifelseTask1

---
+++ src/com/demo/firstclass/DatatypesDemo1.java
@@ -0,0 +1,43 @@
+package com.demo.firstclass;
+
+public class DatatypesDemo1 {
+	
+	public static void main(String[] args) {
+	
+		//Create a Variable
+		
+		//Decalare a Vraible
+		
+		int a = 0 ;//0
+		
+		System.out.println(a);
+		
+		a = 10 ;//Reintailze
+		
+		System.out.println(a);
+				
+		byte b = 127 ;//128
+		
+		System.out.println(b);
+		
+        b = 20 ;//Reintailze
+		
+		System.out.println(b);
+		
+		short c = 12345 ;
+		
+		System.out.println(c);
+		
+		long d = 1234567890 ;
+		
+		System.out.println(d);
+		
+		float e = 1234567.1234567890f ;
+		
+		System.out.println(e);
+				
+		double f = 1234567890;
+		
+        System.out.println(f);
+	}
+}
