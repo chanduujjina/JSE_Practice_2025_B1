c01be23db5eab40a81a096a1134778d12df90bba
Abhiram <abhirampiratla123@gmail.com>
2025-06-08 14:18:49 +0530

    Reverse a number

---
+++ src/com/demo/Assignment1/reverse_a_number.java
@@ -0,0 +1,16 @@
+package com.demo.Assignment1;
+import java.util.*;
+public class reverse_a_number {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int res = 0;
+		while(num > 0){
+			res = res * 10 + num%10;
+			num = num/10;
+		}
+		System.out.print(res);
+		sc.close();
+	}
+
+}
