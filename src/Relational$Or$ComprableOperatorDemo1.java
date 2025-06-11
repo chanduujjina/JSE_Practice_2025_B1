e535b52fbe2ae866bee0b65dcfe6e5a078ef388b
DurgaGaneshMedisetti <ganeshmsvd4@gmail.com>
2025-06-11 16:05:08 +0530

    Relational$Or$ComprableOperatorDemo1

---
+++ src/Relational$Or$ComprableOperatorDemo1.java
@@ -0,0 +1,65 @@
+
+public class Relational$Or$ComprableOperatorDemo1 {
+	
+	public static void main(String[] args) {
+	
+		 // Relational$Or$ComprableOperator
+		
+	    int a = 10 ;
+	    
+	    int b = 10 ;
+	    
+        int c = 10 ;
+	    
+	    int d = 20 ;
+	    
+		boolean e = a == b ; // Equal Operator	
+		
+		System.out.println( "Equal Operator = " + e );
+		            
+		boolean f = c == d ; // Equal Operator	
+		
+		System.out.println( "Equal Operator = " + f );
+		
+        boolean g = a != b ; // Not Equal Operator	
+		
+		System.out.println( "Not Equal Operator = " + g );
+		          
+		boolean h = c != d ; // Not Equal Operator	
+		
+		System.out.println( "Not Equal Operator = " + h );
+			
+        boolean i = c > d ; // Greater Then Operator	
+		
+		System.out.println( "Greater Then Operator = " + i );
+		
+        boolean j = d > c ; // Greater Then Operator	
+		
+		System.out.println( "Greater Then Operator = " + j );
+		
+        boolean k = c < d ; // Less Then Operator	
+		
+		System.out.println( "Less Then Operator = " + k );
+		
+        boolean l = d < c ; // Less Then Operator	
+		
+		System.out.println( "Less Then Operator = " + l );
+		
+        boolean m = a >= b ; // Greater Then OR Equal Operator	
+		
+		System.out.println( "Greater Then OR Equal Operator = " + m );
+		            
+		boolean n = c >= d ; // Greater Then OR Equal Operator	
+		
+		System.out.println( "Greater Then OR Equal Operator = " + n );
+		
+        boolean o = a <= b ; // Less Then OR Equal Operator	
+		
+		System.out.println( "Less Then OR Equal Operator = " + o );
+		            
+		boolean p = d <= c ; // Less Then OR Equal Operator	
+		
+		System.out.println( "Less Then OR Equal Operator = " + p );
+				
+			}
+}
