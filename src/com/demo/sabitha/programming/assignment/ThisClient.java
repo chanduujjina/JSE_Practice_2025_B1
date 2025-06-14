d8a695b1be504c8b186391bc458ef94899b64e1d
sabitha <dokkusabitha06@gmail.com>
2025-06-14 17:14:47 +0530

    Assignment1

---
+++ src/com/demo/sabitha/programming/assignment/ThisClient.java
@@ -0,0 +1,10 @@
+package com.demo.sabitha.programming.assignment;
+
+public class ThisClient {
+	public static void main(String[] args) {
+		ThisKeyword thiskeyword=new ThisKeyword("sai");
+		thiskeyword.m1("sree");
+		thiskeyword.setStr("sabitha");
+		System.out.println(thiskeyword.getStr());
+	}
+}
